ef2dced11f26ba8f4060f6634673e6413033e11e
Gustav Tonér <gustav.toner@gmail.com>
2016-05-24 21:14:25 +0200

    Remove import of old routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
-import Home from './Home'
-import CounterRoute from './Counter'
 import TogglRoute from './Toggl'
 
 /*  Note: Instead of using JSX, we recommend using react-router
