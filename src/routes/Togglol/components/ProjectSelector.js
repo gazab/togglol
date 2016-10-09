b5280332cf99f13e80a8010d4f77fe92f2bd580b
Gustav Tonér <gustav.toner@gmail.com>
2016-10-09 17:01:51 +0200

    Build dropdown from clients and projects

--- src/routes/Togglol/components/ProjectSelector.js
+++ src/routes/Togglol/components/ProjectSelector.js
@@ -1,20 +1,95 @@
 import React from 'react'
 
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
+import Select from 'react-select-plus';
+import 'react-select-plus/dist/react-select-plus.css';
 
-var options = [
-    { value: 'one', label: 'One' },
-    { value: 'two', label: 'Two' }
-];
+var options = [{
+	label: 'Black',
+	value: 'black',
+}, {
+	label: 'Primary Colors',
+	options: [{
+		label: 'Yellow',
+		value: 'yellow'
+	}, {
+		label: 'Red',
+		value: 'red'
+	}, {
+		label: 'Blue',
+		value: 'blue'
+	}]
+}, {
+	label: 'Secondary Colors',
+	options: [{
+		label: 'Orange',
+		value: 'orange'
+	}, {
+		label: 'Purple',
+		options: [{
+			label: 'Light Purple',
+			value: 'light_purple'
+		}, {
+			label: 'Medium Purple',
+			value: 'medium_purple'
+		}, {
+			label: 'Dark Purple',
+			value: 'dark_purple'
+		}]
+	}, {
+		label: 'Green',
+		value: 'green'
+	}]
+}, {
+	label: 'White',
+	value: 'white',
+}];
 
 class ProjectSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-         project: undefined
+         project: undefined,
+         options: this.createOptionsList()
     }
   }
+
+  createOptionsList() {
+
+    var groupedProjects = [];
+
+    this.props.projects.forEach(function(project){
+        if(project.cid != undefined)
+        {
+          if(groupedProjects[project.cid] == null)
+            groupedProjects[project.cid] = [];
+
+          groupedProjects[project.cid].push({label: project.name, value: project.id});
+        }
+    });
+
+    var options = [];
+    let that = this;
+    Object.keys(groupedProjects).forEach(function(key) {
+      var values = groupedProjects[key];
+      options.push({label: that.getClient(key).name, options: values})
+    });
+
+    return options;
+  }
+
+  getClient(id) {
+        let retVal = null;
+
+        this.props.clients.forEach(function(client) {
+            if(client.id == id)
+            {
+                retVal = client;
+                return;
+            }
+            
+        });
+        return retVal;
+    }
 
   logChange(val) {
     console.log("Selected: " + val);
@@ -27,7 +102,7 @@ class ProjectSelector extends React.Component {
           <Select
             name="form-field-name"
             value="one"
-            options={options}
+            options={this.state.options}
             onChange={this.logChange}
           />
       </div>
