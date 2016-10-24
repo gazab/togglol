9a0f1c3bccc8ca7ade32cb28f1b4121b09650290
Gustav Tonér <gustav.toner@gmail.com>
2016-10-24 10:32:45 +0200

    :sparkles: Add GitHub star button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,8 @@ export const Header = () => (
     <div className="col-sm-12 text-xs-center">
       <h1 className="display-4">Togglol</h1>
       <p className="lead">When you need to use Toggl a bit differently</p>
-      <a href="https://github.com/gazab/togglol" target="_blank">Visit Togglol on Github</a>
+      <a href="https://github.com/gazab/togglol" target="_blank">Visit Togglol on Github</a>&nbsp;-&nbsp; 
+      <iframe style={{border: '0', position: 'relative', top: '4px'}} src="https://ghbtns.com/github-btn.html?user=gazab&repo=togglol&type=star&count=true" frameborder="0" scrolling="0" width="90px" height="20px"></iframe>
     </div>
   </div>
 )
