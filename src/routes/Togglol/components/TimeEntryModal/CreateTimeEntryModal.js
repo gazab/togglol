acd451e83a2b232e2d990ccd16f5158f187019f8
Gustav Tonér <gustav.toner@consid.se>
2017-03-24 08:30:10 +0100

    :mute: Remove console.log

--- src/routes/Togglol/components/TimeEntryModal/CreateTimeEntryModal.js
+++ src/routes/Togglol/components/TimeEntryModal/CreateTimeEntryModal.js
@@ -62,10 +62,15 @@ class CreateTimeEntryModal extends React.Component {
         
     }
 
+    onKeyPress(event)  {
+        if(event.key == 'Enter') { 
+            console.log("Enter pressed!")
+        } 
+    }
+
     isProjectBillable(projectId)
     {
         const project = this.getProject(projectId);
-        console.log(project);
         return project.billable;
     }
 
@@ -181,7 +186,7 @@ class CreateTimeEntryModal extends React.Component {
                                 <label className="col-sm-2 col-form-label">Description </label>
                                 <div className="col-sm-10">
                                     <p className="form-control-static">
-                                        <input type="text" value={this.state.description} className="form-control" onChange={(event) => this.changeDescription(event.target.value)}/>    
+                                        <input type="text" value={this.state.description} className="form-control" onChange={(event) => this.changeDescription(event.target.value)} onKeyPress={this.onKeyPress} />    
                                     </p>
                                 </div>
                             </div>
