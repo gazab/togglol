ea5636becf072e72c433fd649094abbc52868cb4
Gustav Tonér <gustav.toner@gmail.com>
2017-04-17 18:11:56 +0200

    :lipstick: Rollback to old header style

--- src/routes/Togglol/components/ApiKeyInput.js
+++ src/routes/Togglol/components/ApiKeyInput.js
@@ -35,7 +35,7 @@ class ApiKeyInput extends React.Component {
                             <button className="btn btn-primary" onClick={this.handleLoadPress.bind(this)}>Login</button>
                         </span>
                     </div>
-                    <div className="text-xs-center">
+                    <div className="text-center">
                         <a target="_blank" href="https://toggl.com/app/profile">Find your API token here</a>
                         <p><small>Disclaimer: We are NOT affiliated with Toggl in ANY way</small></p>
                     </div>
