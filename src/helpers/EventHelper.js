302f0626cf99f2ed3c038b4886bbe53a9e45e683
Gustav Tonér <gustav.toner@gmail.com>
2017-12-09 23:13:25 +0100

    Add functions to calc totalt duration for day

--- src/helpers/EventHelper.js
+++ src/helpers/EventHelper.js
@@ -1,10 +1,21 @@
 import moment from 'moment';
-require("moment-duration-format");
+require('moment-duration-format');
 
-export function getEventsForDay(events, day) {
-    
+export function getEventsForDay (events, day) {
+    console.log(events);
+    console.log(day);
+    return events.filter(event => event.start.toDateString() === day.toDateString());
 }
 
-export function getTotalDurationForEvents(events) {
-    return Math.floor(Math.random()*10);
+export function getTotalDurationForEvents (events) {
+    if (events === undefined) return 0;
+
+    let durationSeconds = 0;
+    events.forEach(event => {
+        const startDate = moment(event.start);
+        const endDate = moment(event.end);
+
+        durationSeconds += endDate.diff(startDate, 'milliseconds');
+    });
+    return moment.duration(durationSeconds).asHours();
 }
