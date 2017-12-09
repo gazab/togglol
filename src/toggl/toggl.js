2eea36f9b320a17b1ebc297ed324bc0b06c3c26d
Gustav Tonér <gustav.toner@gmail.com>
2017-12-09 22:50:08 +0100

    Lint toggl.js

--- src/toggl/toggl.js
+++ src/toggl/toggl.js
@@ -1,20 +1,19 @@
 import moment from 'moment';
-require("moment-duration-format");
+require('moment-duration-format');
 
-export function createTogglEntry(id, description, projectId, startDate, endDate, billable)
-{
+export function createTogglEntry (id, description, projectId, startDate, endDate, billable) {
     startDate = moment(startDate);
     endDate = moment(endDate);
 
     var entry = {
-            id: id,
-            description: description,
-            pid: projectId,
-            start: startDate.toISOString(),
-            duration: endDate.diff(startDate, 'seconds'),
-            billable: billable,
-            created_with: "Togglol"
-        };
+        id: id,
+        description: description,
+        pid: projectId,
+        start: startDate.toISOString(),
+        duration: endDate.diff(startDate, 'seconds'),
+        billable: billable,
+        created_with: 'Togglol'
+    };
 
     return entry;
 }
